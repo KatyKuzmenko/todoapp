00b12cd3c6bf8ec105b1569c2592bfbe19d4cd2f
Katy Kuzmenko <rina.punina@gmail.com>
2021-11-11 11:48:29 +0200

    filters added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # todoapp
+1. Add todos DONE
+2. Remove todos DONE
+3. Change status  DONE
+4. Calculate not completed todos DONE
+5. Remove all completed todos  DONE
+6. Filters DONE
+7. Footer should be hidden if no todos added DONE
--- main.js
+++ main.js
@@ -1,39 +1,27 @@
 'use strict';
 
 const root = document.querySelector('.todoapp');
-const todoList = root.querySelector('.todo-list');
 const newTodoField = root.querySelector('.new-todo');
+const todoList = root.querySelector('.todo-list');
+const todos = root.querySelectorAll('.todo-list__item');
 const allToggler = root.querySelector('.toggle-all');
 const clearButton = root.querySelector('.clear-completed');
+const filter = root.querySelector('.filters');
+const footer = root.querySelector('.footer');
+
+footer.hidden = true;
 
 function countNotCompletedTodos() {
   const notCompletedTodos = root.querySelectorAll('.toggle:not(:checked)');
   const counter = root.querySelector('.todo-count');
   const completedTodos = root.querySelectorAll('.toggle:checked');
+  const todos = root.querySelectorAll('.todo-list__item');
 
   counter.innerHTML = `${notCompletedTodos.length} items left`;
   allToggler.checked = notCompletedTodos.length === 0;
   clearButton.hidden = completedTodos.length === 0;
-}
-
-clearButton.addEventListener('click', () => {
-  const completedTodos = root.querySelectorAll('.toggle:checked');
-
-  for (const toggler of completedTodos) {
-    toggler.closest('.todo-list__item').remove();
-  }
-
-  countNotCompletedTodos();
-});
-
-allToggler.addEventListener('change',  () => {
-  const togglers = root.querySelectorAll('.toggle');
-
-  for (const toggler of togglers) {
-    toggler.checked = allToggler.checked;
-    toggler.closest('.todo-list__item').classList.toggle('completed', allToggler.checked);
-  }
-});
+  footer.hidden = todos.length === 0;
+};
 
 newTodoField.addEventListener('keydown', (event) => {
   const id = +new Date();
@@ -54,6 +42,15 @@ newTodoField.addEventListener('keydown', (event) => {
   countNotCompletedTodos();
 });
 
+todoList.addEventListener('change', (event) => {
+  if (!event.target.matches('.toggle')) {
+    return;
+  }
+
+  event.target.closest('.todo-list__item').classList.toggle('completed', event.target.checked);
+  countNotCompletedTodos();
+});
+
 todoList.addEventListener('click', (event) => {
   if (!event.target.matches('.destroy')) {
     return;
@@ -63,13 +60,54 @@ todoList.addEventListener('click', (event) => {
   countNotCompletedTodos();
 });
 
-todoList.addEventListener('change', (event) => {
-  if (!event.target.matches('.toggle-all')) {
-    return;
+allToggler.addEventListener('change',  () => {
+  const togglers = root.querySelectorAll('.toggle');
+
+  for (const toggler of togglers) {
+    toggler.checked = allToggler.checked;
+    toggler.closest('.todo-list__item').classList.toggle('completed', allToggler.checked);
   }
 
-  event.target.closest('.todo-list__item').classList.toggle('completed', event.target.checked);
   countNotCompletedTodos();
 });
 
+clearButton.addEventListener('click', () => {
+  const completedTodos = root.querySelectorAll('.toggle:checked');
 
+  for (const toggler of completedTodos) {
+    toggler.closest('.todo-list__item').remove();
+  }
+
+  countNotCompletedTodos();
+});
+
+filter.addEventListener('click', (event) => {
+  if (!event.target.dataset.filter) {
+    return;
+  }
+
+  const filterLinks = root.querySelectorAll('[data-filter]');
+
+  for (const link of filterLinks) {
+    link.classList.toggle('selected', event.target === link);
+  }
+
+  const togglers = root.querySelectorAll('.toggle');
+  for (const toggler of togglers) {
+    const todo = toggler.closest('.todo-list__item');
+
+    switch (event.target.dataset.filter) {
+      case 'all':
+        todo.hidden = false;
+        break;
+
+      case 'active':
+        todo.hidden = toggler.checked;
+        break;
+
+      case 'completed':
+        todo.hidden = !toggler.checked;
+        break;
+    }
+  }
+});
